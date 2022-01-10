248b407cf294360ddc791e811b13352a7497d257
Jonas Carpay <jonascarpay@gmail.com>
2022-01-10 18:20:40 +0900

    Less simple test bench

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 import Control.Monad
 import Data.Foldable
+import Data.Maybe
 import Data.Ord
 import Lib
 import Solvers
@@ -9,21 +10,27 @@ main :: IO ()
 main = do
   valid <- loadWordList "./valid.txt"
   answers <- loadWordList "./words.txt"
-  printLengthStats simple (Dictionary valid answers)
+  let dict = Dictionary valid answers
+  evalSolver simple dict (parseWord "raise") (fromJust $ parseWord "query")
 
-printLengthStats :: Solver -> Dictionary -> IO ()
-printLengthStats solver dict = do
+evalSolver :: Solver -> Dictionary -> Maybe Word' -> Word' -> IO ()
+evalSolver s dict mfirst ans = do
+  first <- case mfirst of
+    Nothing ->
+      let g = s dict mempty
+       in g <$ putStrLn ("First guess: " <> showWord g)
+    Just g -> g <$ putStrLn ("First guess (forced): " <> showWord g)
+  putStrLn $ unwords $ fmap showWord $ playPure s dict ans (Just first)
+  let g = flip fmap (answerWords dict) $ \ans ->
+        let guesses = playPure simple dict ans (parseWord "raise")
+         in (length guesses, guesses)
+      (n, ws) = maximumBy (comparing fst) g
+      av = average (fmap fst g)
   putStrLn $ "Average: " <> show av
   putStrLn $ "Worst (" <> show n <> "): " <> unwords (fmap showWord ws)
-  where
-    g = flip fmap (answerWords dict) $ \ans ->
-      let guesses = playPure simple dict ans (parseWord "raise")
-       in (length guesses, guesses)
-    (n, ws) = maximumBy (comparing fst) g
-    av = average (fmap fst g)
 
-    average :: [Int] -> Double
-    average xs = fromIntegral (sum xs) / fromIntegral (length xs)
+average :: [Int] -> Double
+average xs = fromIntegral (sum xs) / fromIntegral (length xs)
 
 parseWordIO :: String -> IO Word'
 parseWordIO w = maybe (Sys.die $ "Couldn't parse " <> show w) pure $ parseWord w
--- src/Solvers.hs
+++ src/Solvers.hs
@@ -15,8 +15,17 @@ simple :: Solver
 simple (Dictionary _ answers) know = flip minimumOn candidates $ \guess ->
   let n = sum $ do
         act <- candidates
-        let know' = rate act guess
+        let know' = know <> rate act guess
         pure $ length $ filter (fits know') candidates
    in n
   where
     candidates = filter (fits know) answers
+
+smart :: Solver
+smart (Dictionary valids answers) k =
+  case answers' of
+    [a] -> a
+    _ -> flip minimumOn valids $ \guess ->
+      sum [length $ filter (fits (rate act guess)) answers' | act <- answers']
+  where
+    answers' = filter (fits k) answers
